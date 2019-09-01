cbedc63645e30a24d32cfbbb80929a96e1395d4b
Lauren West <laurenwest12@gmail.com>
2019-09-01 12:32:33 -0400

    seed refresh

--- server/db/seed.js
+++ server/db/seed.js
@@ -30,7 +30,7 @@ const syncAndSeed = () => {
         imgUrl: 'https://i.imgur.com/UzILALO.jpg'
       }),
       Cast.create({
-        id: 18,
+        id: 6,
         name: 'Dan',
         imgUrl: 'https://i.imgur.com/tAt30E0.jpg'
       }),
