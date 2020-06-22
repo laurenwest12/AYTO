5e3d010c2d2bb2679752a6cc5e89ad6a0018f7fd
Lauren West <laurenwest12@gmail.com>
2020-06-22 18:45:35 -0400

    updated ceremony route so that pairs are added

--- models/Pair.js
+++ models/Pair.js
@@ -5,6 +5,8 @@ const Pair = new Schema({
   _pair1: { type: Schema.Types.ObjectId, ref: 'Cast' },
   _pair2: { type: Schema.Types.ObjectId, ref: 'Cast' },
   match: Boolean,
+  _ceremony: { type: Schema.Types.ObjectId, ref: 'Ceremony' },
+  _truthBooth: { type: Schema.Types.ObjectId, ref: 'Truth Booth' },
 });
 
-mongoose.model('pair', Pair);
+mongoose.model('Pair', Pair);
--- routes/ceremonyRoutes.js
+++ routes/ceremonyRoutes.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+const Cast = mongoose.model('Cast');
+const Pair = mongoose.model('Pair');
 const Ceremony = mongoose.model('Ceremony');
 
 module.exports = (app) => {
@@ -30,5 +32,27 @@ module.exports = (app) => {
       req.body
     );
     res.send(ceremony);
+  });
+
+  app.get('/api/ceremonies/:number/pairs', async (req, res) => {
+    const ceremony = await Ceremony.find({ number: req.params.number });
+    const pairs = ceremony._pairs;
+    res.send(pairs);
+  });
+
+  app.post('/api/ceremonies/:number/pairs', async (req, res) => {
+    const { _pair1, _pair2 } = req.body;
+    let match;
+
+    _pair1._match.id === _pair2.id ? (match = true) : (match = false);
+
+    const pair = new Pair({
+      _pair1: await Cast.findOne({ _id: _pair1._id }),
+      _pair2: await Cast.findOne({ _id: _pair2._id }),
+      match,
+      _ceremony: await Ceremony.findOne({ number: req.params.number }),
+    });
+    pair.save();
+    res.send(pair);
   });
 };
