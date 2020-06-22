fe77cb0d10da3af9bf3867430afe0b144c3e5385
Lauren West <laurenwest12@gmail.com>
2020-06-22 18:03:55 -0400

    added cast routes

--- routes/castRoutes.js
+++ routes/castRoutes.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Cast = mongoose.model('Cast');
 
 module.exports = (app) => {
-  app.get('/api/cast', (req, res) => {
-    res.json(Cast);
+  app.get('/api/cast', async (req, res) => {
+    const cast = await Cast.find();
+    res.json(cast);
   });
 
   app.post('/api/cast', async (req, res) => {
@@ -15,6 +16,18 @@ module.exports = (app) => {
     });
 
     await cast.save();
+    res.send(cast);
+  });
+
+  app.put('/api/cast/:id', async (req, res) => {
+    const cast = await Cast.updateOne({ _id: req.body._id }, req.body);
+    res.send(cast);
+  });
+
+  app.put('/api/cast', async (req, res) => {
+    const cast = await req.body.map(async (member) => {
+      await Cast.updateOne({ _id: member._id }, member);
+    });
     res.send(cast);
   });
 };
