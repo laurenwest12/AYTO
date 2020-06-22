810e647072e7b953438110cffc6100e622a37782
Lauren West <laurenwest12@gmail.com>
2020-06-22 18:21:24 -0400

    removed ceremony comments

--- routes/ceremonyRoutes.js
+++ routes/ceremonyRoutes.js
@@ -31,27 +31,4 @@ module.exports = (app) => {
     );
     res.send(ceremony);
   });
-
-  // app.post('/api/cast', async (req, res) => {
-  //   const { name, imgUrl } = req.body;
-  //   const cast = new Cast({
-  //     name,
-  //     imgUrl,
-  //   });
-
-  //   await cast.save();
-  //   res.send(cast);
-  // });
-
-  // app.put('/api/cast/:id', async (req, res) => {
-  //   const cast = await Cast.updateOne({ _id: req.body._id }, req.body);
-  //   res.send(cast);
-  // });
-
-  // app.put('/api/cast', async (req, res) => {
-  //   const cast = await req.body.map(async (member) => {
-  //     await Cast.updateOne({ _id: member._id }, member);
-  //   });
-  //   res.send(cast);
-  // });
 };
